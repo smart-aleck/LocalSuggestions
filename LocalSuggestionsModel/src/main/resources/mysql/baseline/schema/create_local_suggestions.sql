b5a3cc1a576227b8472d80c0648cb84b53ec862c
aliasghar <aliasghar.ashiqhussain@gmail.com>
2017-05-04 00:08:02 +0100

    Committing schemas local_suggestions and local_suggestions_users

---
+++ LocalSuggestionsModel/src/main/resources/mysql/baseline/schema/create_local_suggestions.sql
@@ -0,0 +1,203 @@
+-- Dump completed on 2017-05-03 23:44:16
+CREATE DATABASE  IF NOT EXISTS `local_suggestions` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `local_suggestions`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: local_suggestions
+-- ------------------------------------------------------
+-- Server version	5.7.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `action`
+--
+
+DROP TABLE IF EXISTS `action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `action` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `actionText` varchar(45) DEFAULT NULL COMMENT 'Action: STAR | BOOKMARK | UPVOTE',
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `attachment`
+--
+
+DROP TABLE IF EXISTS `attachment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attachment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `suggestionId` bigint(20) NOT NULL,
+  `attachmentType` varchar(45) DEFAULT NULL COMMENT 'Attachment Type: Image | Document | PDF | Excel',
+  `attachmentName` varchar(100) DEFAULT NULL,
+  `attachment` blob,
+  `attachmentMetaData` json DEFAULT NULL COMMENT 'Meta Data: Json structure of meta data information',
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `suggestion-attachmentId-id_idx` (`suggestionId`),
+  CONSTRAINT `suggestion-attachmentId-id` FOREIGN KEY (`suggestionId`) REFERENCES `suggestion` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `audit`
+--
+
+DROP TABLE IF EXISTS `audit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `audit` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) DEFAULT NULL,
+  `ip` varchar(45) DEFAULT NULL,
+  `device` varchar(45) DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `location` point DEFAULT NULL,
+  `activity` varchar(45) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `suggestionId` bigint(20) NOT NULL,
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `content` text NOT NULL,
+  `location` point DEFAULT NULL,
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `suggestion-comment-id_idx` (`suggestionId`),
+  CONSTRAINT `suggestion-comment-id` FOREIGN KEY (`suggestionId`) REFERENCES `suggestion` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `suggestion`
+--
+
+DROP TABLE IF EXISTS `suggestion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suggestion` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `subject` varchar(100) NOT NULL,
+  `content` text,
+  `expirationTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `suggestionType` varchar(45) DEFAULT NULL COMMENT 'SuggestionType: QUESTION | POST',
+  `category` varchar(45) DEFAULT NULL,
+  `location` point NOT NULL,
+  `displayLocation` point DEFAULT NULL COMMENT 'For user privacy i.e. Location on screen is different to the one actually posted',
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `suggestion_action`
+--
+
+DROP TABLE IF EXISTS `suggestion_action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suggestion_action` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `suggestionId` bigint(20) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `actionId` int(11) NOT NULL COMMENT 'Action: STAR | BOOKMARK | UPVOTE',
+  `location` point DEFAULT NULL,
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `suggestion-suggestion_action-id_idx` (`suggestionId`),
+  KEY `action-suggestion_action-id_idx` (`actionId`),
+  CONSTRAINT `action-suggestion_action-id` FOREIGN KEY (`actionId`) REFERENCES `action` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `suggestion-suggestion_action-id` FOREIGN KEY (`suggestionId`) REFERENCES `suggestion` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `suggestion_tag`
+--
+
+DROP TABLE IF EXISTS `suggestion_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suggestion_tag` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `suggestionId` bigint(20) NOT NULL,
+  `tag` varchar(45) NOT NULL,
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `suggestion-suggestion_tag-id_idx` (`suggestionId`),
+  CONSTRAINT `suggestion-suggestion_tag-id` FOREIGN KEY (`suggestionId`) REFERENCES `suggestion` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_decoration_override`
+--
+
+DROP TABLE IF EXISTS `user_decoration_override`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_decoration_override` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `decorationId` int(11) NOT NULL COMMENT 'User access will be part of the decoration',
+  `decorationName` varchar(45) DEFAULT NULL COMMENT 'User Decoration (e.g. BASIC)\nlocationUpdate: True \nsendSuggestions: 3/month\ndefaultSuggestionExpiration: 1week\npromotedSuggestions: 0/month',
+  `defaultValue` varchar(45) DEFAULT NULL,
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_decoration-user_decoration_override-user_idx` (`decorationName`,`decorationId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-03 23:44:16
---
+++ LocalSuggestionsModel/src/main/resources/mysql/baseline/schema/create_local_suggestions_users.sql
@@ -0,0 +1,163 @@
+CREATE DATABASE  IF NOT EXISTS `local_suggestions_users` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `local_suggestions_users`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: local_suggestions_users
+-- ------------------------------------------------------
+-- Server version	5.7.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `access`
+--
+
+DROP TABLE IF EXISTS `access`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `access` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `accessText` varchar(45) DEFAULT NULL,
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `addressType` varchar(45) NOT NULL,
+  `houseNumber` varchar(45) DEFAULT NULL,
+  `addressLine1` varchar(45) DEFAULT NULL,
+  `addressLine2` varchar(45) DEFAULT NULL,
+  `addressLine3` varchar(45) DEFAULT NULL,
+  `addressLine4` varchar(45) DEFAULT NULL,
+  `zipOrPostCode` varchar(45) DEFAULT NULL,
+  `countyOrProvince` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user-address-id_idx` (`userId`),
+  CONSTRAINT `user-address-id` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `decoration`
+--
+
+DROP TABLE IF EXISTS `decoration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `decoration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `accessId` int(11) NOT NULL COMMENT 'User Access: BASIC | PRO | ADMIN',
+  `decorationName` varchar(45) DEFAULT NULL COMMENT 'User Decoration (e.g. BASIC)\nlocationUpdate: True \nsendSuggestions: 3/month\ndefaultSuggestionExpiration: 1week\npromotedSuggestions: 0/month',
+  `defaultValue` varchar(45) DEFAULT NULL,
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_access-user_decoration-id_idx` (`accessId`),
+  CONSTRAINT `user_access-user_decoration-id` FOREIGN KEY (`accessId`) REFERENCES `access` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `phone`
+--
+
+DROP TABLE IF EXISTS `phone`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phone` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `phoneType` varchar(45) NOT NULL,
+  `phoneNumber` varchar(45) NOT NULL,
+  `phoneCountry` varchar(8) DEFAULT NULL COMMENT 'The country code i.e. +44, +92',
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user-phone-id_idx` (`userId`),
+  CONSTRAINT `user-phone-id` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `profileType` varchar(45) NOT NULL COMMENT 'There will be a default ''Local Suggestion profile''. Other profiles can be Facebook, Google etc.',
+  `profilePicture` blob,
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user-profile-id_idx` (`userId`),
+  CONSTRAINT `user-profile-id` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `displayName` varchar(100) DEFAULT NULL,
+  `firstName` varchar(45) DEFAULT NULL,
+  `lastName` varchar(45) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `passwordHash` varchar(256) NOT NULL,
+  `dateJoined` date DEFAULT NULL,
+  `userAccessId` int(11) NOT NULL COMMENT 'User Access: BASIC | PRO | ADMIN',
+  `version` int(11) NOT NULL,
+  `updateTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_access-user-id_idx` (`userAccessId`),
+  CONSTRAINT `user_access-user-id` FOREIGN KEY (`userAccessId`) REFERENCES `access` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-03 23:44:16
