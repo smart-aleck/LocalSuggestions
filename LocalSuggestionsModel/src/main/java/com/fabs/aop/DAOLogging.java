baf1820777fa5781b71ade9b32b4bfcbcad924fa
aliasghar <aliasghar.ashiqhussain@gmail.com>
2017-05-13 14:16:29 +0100

    aop to handle exceptions

--- LocalSuggestionsModel/src/main/java/com/fabs/aop/DAOLogging.java
+++ LocalSuggestionsModel/src/main/java/com/fabs/aop/DAOLogging.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -25,6 +26,12 @@ public class DAOLogging {
     @AfterReturning(pointcut="execution(* com.fabs.dao..*(..))", returning="result")
     public void after(JoinPoint joinPoint, Object result){
         logger.debug(String.format("[%s] returns %s",
-                joinPoint.getSignature().toShortString(),result));
+                joinPoint.getSignature().toShortString(), result));
+    }
+
+    @AfterThrowing(pointcut="execution(* com.fabs.dao..*(..))", throwing = "exception")
+    public void afterException(JoinPoint joinPoint, Exception exception){
+        logger.error(String.format("[%s] returns %s",
+                joinPoint.getSignature().toShortString(), exception));
     }
 }
--- LocalSuggestionsModel/src/main/java/com/fabs/aop/EncryptionLogging.java
+++ LocalSuggestionsModel/src/main/java/com/fabs/aop/EncryptionLogging.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -26,5 +27,11 @@ public class EncryptionLogging {
     public void after(JoinPoint joinPoint, Object result){
         logger.debug(String.format("[%s] returns %s",
                 joinPoint.getSignature().toShortString(),result));
+    }
+
+    @AfterThrowing(pointcut="execution(* com.fabs.encryption..*(..))", throwing = "exception")
+    public void afterException(JoinPoint joinPoint, Exception exception){
+        logger.error(String.format("[%s] returns %s",
+                joinPoint.getSignature().toShortString(), exception));
     }
 }
